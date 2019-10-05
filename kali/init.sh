8272d8b37751c370acad26b638ab074dcc334486
Anthony Lapenna <lapenna.anthony@gmail.com>
2019-10-05 17:54:20 +1300

    update kali init.sh

--- kali/init.sh
+++ kali/init.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-apt install -y zsh
+apt install -y zsh terminator
 
+# Setup oh-my-zsh
 curl -Lo /tmp/install-zsh.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
 chmod +x /tmp/install-zsh.sh
 sh /tmp/install-zsh.sh
 
-echo "Done"
+# Clean-up server SSH keys
+rm -rf /etc/ssh/ssh_host_*
+dpkg-reconfigure openssh-server
+
+# Password update
+passwd root

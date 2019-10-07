08ad8fad937a21568e794f942180930f8c40c514
Anthony Lapenna <lapenna.anthony@gmail.com>
2019-10-07 19:26:30 +1300

    update kali

---
+++ kali/bluetooth.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt install -y bluetooth blueman
+
+# Bluetooth
+systemctl enable bluetooth
--- kali/init.sh
+++ kali/init.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
 export DEBIAN_FRONTEND=noninteractive
-apt install -y zsh terminator bluetooth blueman
+apt install -y zsh terminator
 
 # Setup oh-my-zsh
 curl -Lo /tmp/install-zsh.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
 chmod +x /tmp/install-zsh.sh
 sh /tmp/install-zsh.sh
 
-# Clean-up server SSH keys
+# Reconfigure SSH
 rm -rf /etc/ssh/ssh_host_*
+mkdir /root/.ssh && chmod 700 /root/.ssh
+cat ../common/ssh/sshkey.pub >> /root/.ssh/authorized_keys && chmod 600 /root/.ssh/authorized_keys
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 dpkg-reconfigure openssh-server
-
-# Bluetooth
-systemctl enable bluetooth
+service sshd restart
 
 # Password update
 passwd root

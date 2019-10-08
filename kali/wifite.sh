fd0c393be22a78743bf6cffeab094581bd64cb49
Anthony Lapenna <lapenna.anthony@gmail.com>
2019-10-08 18:57:53 +1300

    update kali

--- kali/wifite.sh
+++ kali/wifite.sh
@@ -3,17 +3,17 @@
 export DEBIAN_FRONTEND=noninteractive
 apt install -y build-essential libpcap-dev pixiewps cowpatty libopenssl openssl-dev zlib zlib-dev libcurl curl-dev libpthread pthread-dev librt librt-dev
 
-cd /tmp
-git clone https://github.com/derv82/wifite2.git
-cd wifite2
-python setup.py install
-
-cd /tmp
-git clone https://github.com/t6x/reaver-wps-fork-t6x
-cd reaver-wps-fork-t6x/src
-sh configure
-make
-make install
+# cd /tmp
+# git clone https://github.com/derv82/wifite2.git
+# cd wifite2
+# python setup.py install
+#
+# cd /tmp
+# git clone https://github.com/t6x/reaver-wps-fork-t6x
+# cd reaver-wps-fork-t6x/src
+# sh configure
+# make
+# make install
 
 cd /tmp
 git clone https://github.com/ZerBea/hcxdumptool.git
